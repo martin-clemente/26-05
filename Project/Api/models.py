a853799debb2520c4dafe8c66438fc631a34ebbe
martin-clemente <martinmiguel.clemente@gmail.com>
2026-05-26 10:39:17 -0300

    DB

--- Project/Api/models.py
+++ Project/Api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# --> Class + Nombre.
+class Alumnos(models.Model):
+    ID_Alumno=models.AutoField(primary_key=True)
+    DNI=models.TextField(max_lenght=10)
+    Apellido=models.TextField(max_lenght=35)
+    Nombre=models.TextField(max_lenght=35)
+    Edad=models.IntegerField()
+    Calle=models.TextField(max_lenght=50)
+    Altura=models.IntegerField()
+
+    def __str__(self):
+        return self.DNI
